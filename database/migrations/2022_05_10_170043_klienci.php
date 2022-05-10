7ce392410ded2c50fb00d57874883be3f7f77bc8
Bazielich_Tomasz <karoshiplay@gmail.com>
2022-05-10 22:27:52 +0200

    Dodanie tabeli klienci

---
+++ database/migrations/2022_05_10_170043_klienci.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('klienci', function (Blueprint $table) {
+            $table->id();
+            $table->string('imie', 30);
+            $table->string('nazwisko', 40);
+            $table->string('plec', 10);
+            $table->date('data_urodzenia');
+            $table->string('PESEL', 11);
+            $table->string('adres_zamieszkania');
+            $table->string('email', 255);
+            $table->string('telefon', 9);
+            //$table->foreign('pracownicy_id')->references('id')->on('pracownicy');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('klienci');
+    }
+};
