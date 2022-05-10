b5ff17147e4deb44d176f2df56e65eac98ac524a
Bazielich_Tomasz <karoshiplay@gmail.com>
2022-05-10 22:42:04 +0200

    Dodanie tabeli konto_klienta

---
+++ database/migrations/2022_05_10_203126_konto_klienta.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('konto_klienta', function (Blueprint $table) {
+            $table->id();
+            $table->double('saldo');
+            $table->integer('numer');
+            $table->string('login', 255);
+            $table->string('haslo', 255);
+            //$table->foreign('klienci_id')->references('id')->on('klienci');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('konto_klienta');
+    }
+};
